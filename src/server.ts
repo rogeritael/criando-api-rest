4967d8c17621d059383d9f98986cea62e6fd1e36
rogeritael <rogeritael.contato@gmail.com>
2022-04-10 04:26:43 -0300

    criando um CRUD utilizando a API REST

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,68 @@
 import express from 'express';
+import { v4 as uuid } from 'uuid';
 
 const app = express();
+app.use(express.json());
 
 app.listen('3333', () =>{
     console.log('started');
 })
 
-// rotas
+interface User{
+    id: string
+    name: string
+    email: string
+}
 
+const users: User[] = [];
+
+
+
+// retornar usuários
 app.get('/users', (req, res)=>{
-    return res.json(['usuário1', 'usuário2']);
+    return res.json(users);
 });
 
+// inserir usuários
 app.post('/users', (req, res)=>{
+    const { name, email } = req.body;
 
+    const user = { id: uuid(), name, email }
+
+    users.push(user);
+
+    return res.json(user);
 });
 
-app.post('/users', (req, res)=>{
+// atualizar usuário
+app.put('/users/:id', (req, res)=>{
+    const { id } = req.params;
+    const { name, email } = req.body;
 
-});
+    const userIndex = users.findIndex((user) => user.id == id);
 
-app.post('/users', (req, res)=>{
+    if(userIndex < 0){
+        return res.status(404).json({error: 'User not found'});
+    }
 
+    const user = {id, name, email};
+    users[userIndex] = user;
+
+    return res.json(user)
+ });
+
+
+//  deleta usuário
+app.delete('/users/:id', (req, res)=>{
+    const { id } = req.params;
+
+    const userIndex = users.findIndex((user) => user.id == id);
+
+    if(userIndex < 0){
+        return res.status(404).json({error: 'User not found'});
+    }
+
+    users.splice(userIndex, 1);
+
+    return res.status(204).send()
 });
